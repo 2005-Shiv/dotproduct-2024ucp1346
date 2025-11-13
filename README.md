dc69553f5c6527f4613a7fe900718c52843a826a
Pranav <2023ucp1686@mnit.ac.in>
2025-11-13 12:28:12 +0530

    Adding vecadd

---
+++ README.md
@@ -0,0 +1,1 @@
+This program multiplies two vectors
---
+++ vecadd.py
@@ -0,0 +1,16 @@
+import numpy as np
+from multiprocessing import Pool
+
+def parallel_dot_product(args):
+    a, b, start, end = args
+    return np.sum(a[start:end] * b[start:end])
+
+def dot_product_parallel(a, b, num_processes=4):
+    n = len(a)
+    chunk_size = n // num_processes
+    args = [(a, b, i * chunk_size, (i + 1) * chunk_size) for i in range(num_processes)]
+    
+    with Pool(num_processes) as pool:
+        result = sum(pool.map(parallel_dot_product, args))
+    return result
+
